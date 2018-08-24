1895f2285f798d32182beb3285fc5112a9b32116
kandacewithak <kandacehobrien@gmail.com>
2018-08-24 13:04:49 -0400

    refresh and link button updates

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -27,32 +27,25 @@ app.getUserResult = (gender, category) => {
             // after the results are returned, create a new variable for the resulting array
 
             const resultArray = res.results;
-            console.log(resultArray);
+            // console.log(resultArray);
 
             // create an empty array for the three random items from Etsy
             
             app.numberGenerator(resultArray);
             app.globalList = resultArray;
 
-            console.log(app.globalList);
-            
-
-           
+            // console.log(app.globalList);
 
         });
-
-        
-        
     };
 
-     // generate three different random numbers to select three items from the resulting array that we will display on the page
+    // generate three different random numbers to select three items from the resulting array that we will display on the page
 
 app.globalList = [];     
 
 app.numberGenerator = function (firstArray) {
 
     const finalResult = [];
-
 
     // we splice the item so that it does not repeat in our results; we used (item1, item1) because it is the required syntax and just (item1) didn't give us the results we wanted
     const randomNumber1 = Math.floor(Math.random() * firstArray.length)
@@ -75,12 +68,6 @@ app.numberGenerator = function (firstArray) {
 
 };
 
-app.changeGenerator = function (array){
-    const newItemNumber = Math.floor(Math.random() * array.length)
-    const newItem = array[newItemNumber];
-    console.log(newItem);
-}
-
 app.showResults = function(array){
     // use the forEach method to display item info on page
     $('.giftContainer').empty();
@@ -90,19 +77,18 @@ app.showResults = function(array){
     array.forEach((item) => {
 
         const giftPiece = $('<div>').addClass('giftResult' + i).addClass('giftResult')
-
-
         const imageDiv = $(`<div class="giftImage${i}">`);
         const image = $('<img>').attr('src', item.Images[0].url_fullxfull).addClass('image' + i);
-
         imageDiv.append(image);
         const title = $('<h2>').addClass('itemTitle' + i).html(item.title);
         const price = $('<h3>').addClass('itemPrice' + i).html(`$${item.price} USD`);
         const change = $('<input type="submit" id="changeItem" value="Change Item">').addClass('changeItem' + i);
+        // link item button here instead of input
+        const linkToItem = $('<input type="submit" id="linkItem" value="Link to Item">').addClass('linkItem' + i);
 
         i++
         
-        giftPiece.append(imageDiv, title, price, change);
+        giftPiece.append(imageDiv, title, price, change, linkToItem);
         $('.giftContainer').append(giftPiece);
         
     });
@@ -116,49 +102,78 @@ $('form').on('submit', function(e){
     app.getUserResult(userGender, userCategory);
 });
 
-
+// generate one random number to change one item
+app.changeGenerator = function (array){
+    const newItemNumber = Math.floor(Math.random() * array.length)
+    const newItem = array[newItemNumber];
+    return newItem;
+}
 
 //change single items
 
 $('.giftContainer').on('click', '.changeItem1', function (e) {
     e.preventDefault();
-    console.log('this works');
 
     const updatedItem = app.changeGenerator(app.globalList);
     console.log(updatedItem);
-    
-    $('.giftContainer .image1').html(`${updatedItem}.Images[0]`);
+
+    $('.giftResult1').empty();
+
+    const updatedImageDiv1 = $(`<div class="giftImage1">`);
+    const updatedImage1 = $('<img>').attr('src', updatedItem.Images[0].url_fullxfull).addClass('image1');
+    updatedImageDiv1.append(updatedImage1);
+    const updatedTitle1 = $('<h2>').addClass('itemTitle1').html(updatedItem.title);
+    const updatedPrice1 = $('<h3>').addClass('itemPrice1').html(`$${updatedItem.price} USD`);
+    const updatedChange1 = $('<input type="submit" id="changeItem" value="Change Item">').addClass('changeItem1');
+    const linkToItem = $('<input type="submit" id="linkItem" value="Link to Item">').addClass('linkItem' + i)
+
+    $('.giftResult1').append(updatedImageDiv1, updatedTitle1, updatedPrice1, updatedChange1);
 })
 
 
 $('.giftContainer').on('click', '.changeItem2', function (e) {
     e.preventDefault();
-    console.log('this works');
-
     const updatedItem = app.changeGenerator(app.globalList);
-    console.log(this);
+    console.log(updatedItem);
 
-    /* updatedItem. */
+    $('.giftResult2').empty();
+
+    const updatedImageDiv2 = $(`<div class="giftImage2">`);
+    const updatedImage2 = $('<img>').attr('src', updatedItem.Images[0].url_fullxfull).addClass('image2');
+    updatedImageDiv2.append(updatedImage2);
+    const updatedTitle2 = $('<h2>').addClass('itemTitle2').html(updatedItem.title);
+    const updatedPrice2 = $('<h3>').addClass('itemPrice2').html(`$${updatedItem.price} USD`);
+    const updatedChange2 = $('<input type="submit" id="changeItem" value="Change Item">').addClass('changeItem2');
+
+    $('.giftResult2').append(updatedImageDiv2, updatedTitle2, updatedPrice2, updatedChange2);
+
 })
 
 
 $('.giftContainer').on('click', '.changeItem3', function (e) {
     e.preventDefault();
-    console.log('this works');
 
     const updatedItem = app.changeGenerator(app.globalList);
-    console.log(this);
+    $('.giftResult3').empty();
 
-    /* updatedItem. */
+    const updatedImageDiv3 = $(`<div class="giftImage3">`);
+    const updatedImage3 =$('<img>').attr('src', updatedItem.Images[0].url_fullxfull).addClass('image3');
+    
+    updatedImageDiv3.append(updatedImage3);
+    const updatedTitle3 = $('<h2>').addClass('itemTitle3').html(updatedItem.title);
+    const updatedPrice3 = $('<h3>').addClass('itemPrice3').html(`$${updatedItem.price} USD`);
+    const updatedChange3 = $('<input type="submit" id="changeItem" value="Change Item">').addClass('changeItem3');
+
+    $('.giftResult3').append(updatedImageDiv3, updatedTitle3, updatedPrice3, updatedChange3);
 })
 
 
 // REFRSH ALL
-// $('.giftContainer').on('click', '#changeItem', function(e){
-//     e.preventDefault();
-//     console.log('it clicked');
-//     app.getUserResult();
-// });
+//Bug at momement, it this button refreshes entire page, may not return down to gift items section
+ $('#submitChange').on('click', function(e){
+    e.preventDefault();
+    app.getUserResult();
+ });
 
 
 
